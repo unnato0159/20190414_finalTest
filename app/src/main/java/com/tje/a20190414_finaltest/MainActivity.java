2596a509876fb5d11243519bdbfa855f74bc24c9
unnato0159 <unnato01@naver.com>
2019-04-14 16:28:50 +0900

    Initial commit

---
+++ app/src/main/java/com/tje/a20190414_finaltest/MainActivity.java
@@ -0,0 +1,55 @@
+package com.tje.a20190414_finaltest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText inputKee;
+    EditText inputKg;
+    Button confirm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        inputKee = findViewById(R.id.inputKee);
+        inputKg = findViewById(R.id.inputKg);
+        confirm = findViewById(R.id.confirm);
+
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                float kee= Integer.parseInt(inputKee.getText().toString());
+               float kg = Integer.parseInt(inputKg.getText().toString());
+
+
+                float result = 0;
+                result =  kg + ( kee*kee);
+                String  keee = "";
+
+                if(result <18.5){
+                    keee = "저체중";
+                }else if(result < 25 && result >23  ){
+                    keee = "정상";
+
+                }else if(result >25 && result <30 ){
+                    keee ="비만";
+
+                }else{
+                    keee="고도비만";
+
+                }
+
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                intent.putExtra("keee",keee);
+            }
+        });
+    }
+}
